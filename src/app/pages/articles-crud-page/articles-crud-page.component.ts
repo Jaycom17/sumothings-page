b30271884a73ca5259d123f7ab1747ece91c9393
Jaycom17 <juanorejuela499@gmail.com>
2024-04-29 17:39:52 -0500

    pages

---
+++ src/app/pages/articles-crud-page/articles-crud-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-articles-crud-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './articles-crud-page.component.html',
+  styleUrl: './articles-crud-page.component.css'
+})
+export class ArticlesCrudPageComponent {
+
+}
