0d6d4b7e25c66de2c974ea7ccfccfe832e0e0433
IyorchsI <joanrosero@unicacua.edu.co>
2024-05-05 12:52:53 -0500

    cambios al listado de artículos-artículo

--- src/app/pages/article-page/article-page.component.ts
+++ src/app/pages/article-page/article-page.component.ts
@@ -1,16 +1,21 @@
-import { Component} from '@angular/core';
-import { ArticlesPageComponent } from '../articles-page/articles-page.component';
+import { Component, Input} from '@angular/core';
 
 @Component({
     selector: 'app-article-page',
     standalone: true,
     templateUrl: './article-page.component.html',
     styleUrl: './article-page.component.css',
-    imports: [ArticlesPageComponent]
+    imports: []
 })
 export class ArticlePageComponent {
-  articulo = {artImg: '', artNombre: 'Esto es una prueba', artFecha: new Date(), artDescripcion: 'Art des asasjhasklasj' }; 
-  inputData($event:any){
-    this.articulo=$event;
+  @Input() articuloI?:{artImg:string, artNombre:string, artFecha:Date, artDescripcion:string};
+  articulo = {artImg: '', artNombre: 'Esto es una prueba', artFecha: new Date(), artDescripcion: 'Art des asasjhasklasj' };
+  
+  cambiarArt():{artImg:string, artNombre:string, artFecha:Date, artDescripcion:string}{
+    if(this.articuloI!=null){
+      this.articulo=this.articuloI;
+    }
+    return this.articulo;
   }
+  
 }
--- src/app/pages/articles-page/articles-page.component.ts
+++ src/app/pages/articles-page/articles-page.component.ts
@@ -1,4 +1,6 @@
-import { Component , Output, EventEmitter} from '@angular/core';
+import { Component} from '@angular/core';
+import { CommonModule, NgIf, NgFor } from '@angular/common';
+import { ArticlePageComponent } from "../article-page/article-page.component";
 
 
 @Component({
@@ -6,9 +8,11 @@ import { Component , Output, EventEmitter} from '@angular/core';
     standalone: true,
     templateUrl: './articles-page.component.html',
     styleUrl: './articles-page.component.css',
-    imports: []
+    imports: [ArticlePageComponent,CommonModule, NgIf, NgFor]
 })
 export class ArticlesPageComponent {
+  btnEstadoC:boolean=false;
+  btnEstadoF:boolean=true;
   articulos = [
     { artImg: '', artNombre: 'SumoArt1', artFecha: new Date("05/02/2024"), artDescripcion: 'Art assaasas'},
     { artImg: '', artNombre: 'SumoArt2', artFecha: new Date("04/02/2024"), artDescripcion: 'Art assaasas'},
@@ -16,11 +20,15 @@ export class ArticlesPageComponent {
     { artImg: '', artNombre: 'SumoArt4', artFecha: new Date("02/02/2024"), artDescripcion: 'Art assaasas'},
     { artImg: '', artNombre: 'SumoArt5', artFecha: new Date("01/02/2024"), artDescripcion: 'Art assaasas'}
   ]
+  articuloS = {artImg: '', artNombre: 'Esto es una prueba', artFecha: new Date(), artDescripcion: 'Art des asasjhasklasj' };
 
-  @Output() articuloEvent = new EventEmitter ();
-  
-  sendEvent(articulo:any){
-    this.articuloEvent.emit(articulo);
+  articuloActual(articulo:{artImg:string, artNombre:string, artFecha:Date, artDescripcion:string}){
+    this.cambiarEstado();
+    this.articuloS=articulo;
+  }
+  cambiarEstado(){
+    this.btnEstadoC=!this.btnEstadoC;
+    this.btnEstadoF=!this.btnEstadoF;
   }
 
 }
