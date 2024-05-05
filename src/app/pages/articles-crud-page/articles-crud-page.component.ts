be5a5dcfcd39e59a9e1d3d285a18e084e585fbfd
IyorchsI <joanrosero@unicacua.edu.co>
2024-05-04 20:28:16 -0500

    cambios crud artículo 3

--- src/app/pages/articles-crud-page/articles-crud-page.component.ts
+++ src/app/pages/articles-crud-page/articles-crud-page.component.ts
@@ -23,10 +23,29 @@ export class ArticlesCrudPageComponent {
     { artImg: '', artNombre: 'SumoArt5', artFecha: new Date("01/02/2024"), artDescripcion: 'Art assaasas'}
   ]
   index=0;
+  articuloAux = {artImg: '', artNombre: '', artFecha: new Date(), artDescripcion: '' };
   articuloR = {artImg: '', artNombre: '', artFecha: new Date(), artDescripcion: '' };
   estado = {Crear: 'Crear',Editar: 'Editar'};
   tag = this.estado.Crear;
+  updateIndex(){
+    this.index=this.index+1;
+  }
   cambiarEstadoC(){
+    this.articuloR=this.articuloAux;
+    this.btn_estadoC=!this.btn_estadoC;
+    this.btn_estadoR=!this.btn_estadoR;
+    this.tag = this.estado.Crear;
+  }
+  cambiarEstadoC2(artImg:string, artNombre:string, artFecha:string, artDescripcion:string){
+    this.articuloR.artImg=artImg;
+    this.articuloR.artNombre=artNombre;
+    this.articuloR.artFecha=new Date(artFecha);
+    this.articuloR.artDescripcion=artDescripcion;
+    if(this.tag==this.estado.Crear){
+      this.agregararticulo(this.articuloR);
+    }else{
+      this.editararticulo(this.articuloR);
+    }
     this.btn_estadoC=!this.btn_estadoC;
     this.btn_estadoR=!this.btn_estadoR;
     this.tag = this.estado.Crear;
@@ -37,9 +56,11 @@ export class ArticlesCrudPageComponent {
     this.articuloR=articulo;
     this.tag=this.estado.Editar;
   }
-
   agregararticulo(articulo:{artImg:string, artNombre:string, artFecha:Date, artDescripcion:string}){
     this.articulos.push(articulo);
+    console.log(this.articulos.join)
+  }
+  editararticulo(articulo:{artImg:string, artNombre:string, artFecha:Date, artDescripcion:string}){
     console.log(this.articulos.join)
   }
 }
