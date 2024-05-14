231a635d34c24e8db3965f832c74a250ef9b9750
Jaycom17 <juanorejuela499@gmail.com>
2024-05-14 07:41:05 -0500

    about-us page was added

--- src/app/pages/about-us-page/about-us-page.component.ts
+++ src/app/pages/about-us-page/about-us-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-about-us-page',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './about-us-page.component.html',
   styleUrl: './about-us-page.component.css'
 })
