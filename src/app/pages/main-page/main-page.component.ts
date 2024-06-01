119f91469f52493f9a73cc06b8871f566f9d147e
Jaycom17 <juanorejuela499@gmail.com>
2024-06-01 11:49:12 -0500

    some mistakes were fixed

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -11,29 +11,5 @@ import { FooterComponent } from '../../components/footer/footer.component';
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent {
-  services = [
-    { title: 'Domótica', description: 'Ofrecemos objetos inteligentes fáciles de instalar y utilizar, para que usted invierta su tiempo en lo importante.', icon: 'https://cdn-icons-png.flaticon.com/128/1165/1165103.png' },
-    { title: 'Entornos Inteligentes IoT', description: 'Creamos entornos de objetos inteligentes que ofrecen servicios ajustados a las necesidades de las personas.', icon: 'https://cdn-icons-png.flaticon.com/128/2640/2640400.png'  },
-    { title: 'Consultoria IT', description: 'Realizamos consultoría en Internet de las Cosas y desarrollo de objetos inteligentes.', icon: 'https://cdn-icons-png.flaticon.com/128/1469/1469884.png'  }
-  ];
-
-  team = [
-    { name: 'Miguel Ángel Niño Zambrano', role: 'CEO', photo: 'https://sumothings.com/sumothings/wp-content/uploads/2022/04/personal_miguel.jpg' },
-    { name: 'Omar Guzmán Alvarez', role: 'SOCIO', photo: 'https://sumothings.com/sumothings/wp-content/uploads/2022/04/La_sala__Marzo_2017-300x300.jpg' },
-    { name: 'Julian Rojas Bolaños', role: 'SOCIO', photo: 'https://sumothings.com/sumothings/wp-content/uploads/2022/04/photo_2022-04-26_16-40-38.jpg' },
-    { name: 'Andrea Pabón Guerrero', role: 'SOCIO', photo: 'https://sumothings.com/sumothings/wp-content/uploads/2022/04/photo_2022-03-19_23-48-26.jpg' }
-  ];
-
-  customOptions: OwlOptions = {
-    loop: true,
-    margin: 10,
-    nav: true,
-    dots: true,
-    responsive: {
-      0: { items: 1 },
-      600: { items: 2 },
-      1000: { items: 3 }
-    }
-  };
   
 }
